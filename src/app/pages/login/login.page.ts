d7dc2f0f5af8500cee04e31d692fbe7cb4852923
Cristian <crist.lopezr@duocuc.cl>
2021-09-19 20:25:34 -0300

    Agrega color duoc y nombre usuario home page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,17 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  usuario={
-    username:'',
-    password:'',
+  usuario = {
+    username: '',
+    password: '',
   }
-  constructor() { }
+  constructor(private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
-  onSubmit(){
-    
+  onSubmit() {
+    if (this.usuario.username === "wacoldo" && this.usuario.password === "asd") {
+      let navExtras: NavigationExtras = {
+        state: {
+          miUsuario: this.usuario
+        }
+      }
+      this.router.navigate(['/home'], navExtras);
+    }
+    else {
+      this.presentAlert();
+    }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Usuario o contraseña incorrectos',
+      message: 'Por favor, ingrese un usuario y contraseña correctos',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
