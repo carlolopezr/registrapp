ad00e5850ab2967625c6fd130c58d82c345d96b3
Cristian <crist.lopezr@duocuc.cl>
2021-11-02 11:24:46 -0300

    agrega qr

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Asignatura } from '../../interfaces/opcionmenu';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -10,29 +12,73 @@ import { Asignatura } from '../../interfaces/opcionmenu';
 })
 export class HomePage {
 
-  nombreUsuario:'';
-  
-  asignaturas:Asignatura[]=[
+  nombreUsuario: '';
+
+  asignaturas: Asignatura[] = [
     {
-    nombre:'Estadistica Descriptiva',
-    codigo:'2421',
-    porcentaje:78,
-    seccion:'004D'
+      nombre: 'Estadistica Descriptiva',
+      codigo: '2421',
+      porcentaje: 78,
+      seccion: '004D'
     },
     {
-      nombre:'Arquitectura de Software',
-      codigo:'ASY4131',
-      porcentaje:100,
-      seccion:'002D', 
+      nombre: 'Arquitectura de Software',
+      codigo: 'ASY4131',
+      porcentaje: 100,
+      seccion: '002D',
     }
-]
-  
-  constructor(private activeRoute: ActivatedRoute, private router:Router) {
-    this.activeRoute.queryParams.subscribe(params =>{
-      if(this.router.getCurrentNavigation().extras.state){
+  ]
+
+  constructor(private activeRoute: ActivatedRoute, private router: Router, private qrScanner: QRScanner, private alertController:AlertController) {
+    this.activeRoute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
         this.nombreUsuario = this.router.getCurrentNavigation().extras.state.miUsuario.username;
       }
     });
   }
 
+  ScanQR() {
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+          this.qrScanner.show();
+          this.showCamera();
+
+          // start scanning
+          let scanSub = this.qrScanner.scan().subscribe(resp => {
+            this.removeCamera();
+            this.qrScanner.hide(); // hide camera preview
+            scanSub.unsubscribe(); // stop scanning
+            this.presentAlert(resp);
+          });
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
+
+  showCamera() {
+    (window.document.querySelector('ion-app') as HTMLElement).classList.add('cameraView');
+  }
+
+  removeCamera() {
+    (window.document.querySelector('ion-app') as HTMLElement).classList.remove('cameraView');
+  }
+
+  async presentAlert(mensaje:string) {
+    const alert = await this.alertController.create({
+      mode: 'ios',
+      header: mensaje,
+      message: 'Escriba otro nombre de usuario',
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+  }
 }
